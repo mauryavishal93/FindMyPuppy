d87af1c7218626901afe6883ac5ab5aa440723bd
Vishal <mauryavishal93@gmail.com>
2026-02-14 18:29:00 +0530

    broken puppy image fix and version update

---
+++ constants/appVersion.ts
@@ -0,0 +1,5 @@
+/**
+ * Single source of truth for app version (Settings, About, etc.).
+ * Bump this when releasing; keep package.json "version" in sync if desired.
+ */
+export const APP_VERSION = '1.0.3';
